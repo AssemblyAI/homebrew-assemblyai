5091a9c39d3aa898aa7fdaef0484e1e34a3af6c2
Francisco Castillo <fcastillo90@gmail.com>
2022-10-17 14:56:30 +0200

    Update assemblyai.rb

--- assemblyai.rb
+++ assemblyai.rb
@@ -55,7 +55,7 @@ class Assemblyai < Formula
   end
 
   def post_install
-    etc.install system("assemblyai welcome -i -v={{ .Tag }} -m=brew")
+    etc.install puts `assemblyai welcome -i -v={{ .Tag }} -m=brew`
   end
 
   test do
